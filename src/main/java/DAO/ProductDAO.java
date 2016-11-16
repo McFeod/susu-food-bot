15e46bcd2473b01c86e509c6dcfd072b5864de58
Ivan Goncharuk <mc.ferden@hotmail.com>
2016-11-16 15:02:58 +0500

    Refactored DAO

--- src/main/java/DAO/ProductDAO.java
+++ src/main/java/DAO/ProductDAO.java
@@ -3,7 +3,6 @@ package DAO;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.ArrayList;
-import java.util.List;
 
 import util.HibernateUtil;
 
@@ -16,37 +15,33 @@ import logic.Product;
 
 public class ProductDAO {
     public void addProduct(Product product) throws SQLException {
-        Session session = null;
+        Session session;
         try {
             session = HibernateUtil.getSessionFactory().getCurrentSession();
             session.beginTransaction();
             session.save(product);
             session.getTransaction().commit();
-            //HibernateUtil.shutdown();
-            if (session != null && session.isOpen()) {
-
-                session.close();
-            }
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e.getMessage(), "Ошибка при вставке", JOptionPane.OK_OPTION);
-        } 
+        }
     }
 
-    public Collection getAllProducts() throws SQLException {
-        Session session = null;
-        List products = new ArrayList<Product>();
+    public Collection<Product> getAllProducts() throws SQLException {
+        Session session;
+        Collection<Product> products = new ArrayList<>();
         try {
-            session = HibernateUtil.getSessionFactory().openSession();
+            session = HibernateUtil.getSessionFactory().getCurrentSession();
+            session.beginTransaction();
             products = session.createCriteria(Product.class).list();
-            //HibernateUtil.shutdown();
+            session.getTransaction().commit();
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e.getMessage(), "Ошибка 'getAll'", JOptionPane.OK_OPTION);
-        } 
+        }
         return products;
     }
 
     public void deleteProduct(Product product) throws SQLException {
-        Session session = null;
+        Session session;
         try {
             session = HibernateUtil.getSessionFactory().getCurrentSession();
             session.beginTransaction();
@@ -54,11 +49,11 @@ public class ProductDAO {
             session.getTransaction().commit();
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e.getMessage(), "Ошибка при удалении", JOptionPane.OK_OPTION);
-        } 
+        }
     }
 
     public Product getProductById(Long product_id) throws SQLException {
-        Session session = null;
+        Session session;
         Product product = null;
         try {
             session = HibernateUtil.getSessionFactory().getCurrentSession();
@@ -67,20 +62,19 @@ public class ProductDAO {
             session.getTransaction().commit();
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e.getMessage(), "Ошибка 'findById'", JOptionPane.OK_OPTION);
-        } 
+        }
         return product;
     }
 
-    public Collection getProductsByName(String name) throws SQLException {
-        Session session = null;
-        List products = new ArrayList<Product>();
+    public Collection<Product> getProductsByName(String name) throws SQLException {
+        Session session;
+        Collection<Product> products = new ArrayList<>();
         try {
             session = HibernateUtil.getSessionFactory().getCurrentSession();
             session.beginTransaction();
-            //Long driver_id = driver.getId();
-            Query query = session.createQuery(" from Product " + " where name = :name ").setString("name", name);
-            // .setLong("name", name);
-            products = (List<Product>) query.list();
+            Query query = session.createQuery(" from Product " + " where name = :name ")
+                    .setString("name", name);
+            products = query.list();
             session.getTransaction().commit();
 
         } catch (Exception e) {
