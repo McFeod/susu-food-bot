98c7b0aee19f8402bc419d8d41297307c6844e0f
Ivan Goncharuk <mc.ferden@hotmail.com>
2016-11-23 08:01:08 +0500

    Исправил ошибки в сообщениях бота

--- src/main/java/api/Texts.java
+++ src/main/java/api/Texts.java
@@ -4,7 +4,7 @@ public class Texts {
 
     public static final String ADVICE_CONFIRMATION      = "След в истории оставлен.";
     public static final String ADVICES_HEADER           = "*Пользователи советуют:*\n";
-    public static final String ADVICE_TEXT_PROMPT       = "Что посоветуете?:";
+    public static final String ADVICE_TEXT_PROMPT       = "Что посоветуете?";
     public static final String CANCEL_CONFIRMATION      = "Отменено.";
     public static final String CANCEL_FAILURE           = "Нечего отменять.";
     public static final String COMPLAIN_CONFIRMATION    = "Жалоба принята.";
@@ -13,8 +13,9 @@ public class Texts {
     public static final String FEEDPOINTS_HEADER        = "*Где можно поесть:*\n";
     public static final String FEEDPOINT_NAME_PROMPT    = "Введите название";
     public static final String FEEDPOINT_PLACE_PROMPT   = "Где это находится? Опишите, пожалуйста.";
-    public static final String HELP_MESSAGE = "/help - список команд для бота.\n" +
-        "/feedpoints - список проверенных мест, можно поесть.\n" +
+    public static final String HELP_MESSAGE =
+        "/help - список команд для бота.\n" +
+        "/feedpoints - список проверенных мест, где можно поесть.\n" +
         "/userfeedpoints - список мест, где можно поесть.\n" +
         "/addfeedpoint <место> - добавить новое место, где можно поесть.\n" +
         "/complain <место> - кто-то наврал вам про буфет? Пожалуйтесь, и мы его найдем.\n" +
@@ -29,6 +30,6 @@ public class Texts {
     public static final String RUNOUT_CONFIRMATION      = "Родина Вас не забудет.";
     public static final String START_PROMPT             = "Привет! Я бот, который поможет узнать, " +
             "где в ЮУрГУ можно перекусить. Наберите /help для ознакомления с моими командами.";
-    public static final String UNEXPECTED_ERROR         = "Ой, всё";
+    public static final String UNEXPECTED_ERROR         = "Ой, всё!";
     public static final String USERFEEDPOINTS_HEADER    = "*Где можно поесть по версии пользователей:*\n";
 }
